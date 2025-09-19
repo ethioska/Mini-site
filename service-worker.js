ade7a9b9d7ca67462e2a14c19ed688629eb38d78
ethioska <ethioska@gmail.com>
2025-09-19 13:12:46 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const cacheName = 'mini-search-cache-v1';
+const assetsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/style.css',
+  '/ads.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll(assetsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
